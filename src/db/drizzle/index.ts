e9cd043d3730d706a0e4ba796132439da417d64f
fernandohsilva1 <fernandohsilva1@hotmail.com>
2026-04-08 20:28:50 -0300

    fix: sqlite directory in deploy

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -1,10 +1,13 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import { postsTable } from './schemas'
 import Database from 'better-sqlite3'
-import { resolve } from 'path'
+import { dirname, resolve } from 'path'
+import { mkdirSync } from 'fs'
 
 const sqliteDatabasePath =
   process.env.SQLITE_PATH || resolve(process.cwd(), 'db.sqlite3')
+mkdirSync(dirname(sqliteDatabasePath), { recursive: true })
+
 const sqliteDatabase = new Database(sqliteDatabasePath)
 
 export const drizzleDb = drizzle(sqliteDatabase, {
